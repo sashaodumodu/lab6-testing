69262bc20fcfc51e92edfc9d0ad6880bb8a84daf
Sasha Odumodu <189272304+sashaodumodu@users.noreply.github.com>
2025-10-02 19:41:24 +0000

    Pushing new change

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -1,6 +1,17 @@
-import pytest
-from presidio_anonymizer.sample import sample_run_anonymizer
+from presidio_anonymizer import sample as sample_mod
 
 def test_sample_run_anonymizer():
-    # replace the following line with your test
-    pass
+    func = getattr(sample_mod, "sample_run_anonymizer", None) or getattr(sample_mod, "run_anonymizer", None) #had to change file
+    assert func is not None, "Something went wrong"
+
+    res = func("My name is Bond.", 11, 15)
+
+    assert res.text == "My name is BIP."
+
+    assert len(res.items) == 1
+    item = res.items[0]
+    assert item.start == 11
+    assert item.end == 14
+    assert item.entity_type == "PERSON"
+    assert item.text == "BIP"
+    assert item.operator == "replace"
